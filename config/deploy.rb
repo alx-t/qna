df3d7174b25222ff4d8d105e08451d19e07a762a
Alex L. Tesevich <atesevich@gmail.com>
2016-02-11 17:16:24 +0300

    lsn-21 unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -109,7 +109,8 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
+      #execute :touch, release_path.join('tmp/restart.txt')
+      invoke 'unicorn:restart'
     end
   end
 
@@ -127,7 +128,7 @@ namespace :deploy do
   after :publishing, :restart
 end
 
-after 'deploy:restart', 'private_pub:start'
+after 'deploy:restart', 'private_pub:restart'
 after 'deploy:restart', 'ts:restart'
 after 'deploy:restart', 'ts:index'
 
